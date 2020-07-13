f8adf224fe0efcbe161e90692f1ac7c6ea44df44
VickentyZherebov <vickenty@yandex.ru>
2020-07-13 20:33:41 +0300

    Создал класс 'Candidate'

--- Clever Staff Rewrite.py
+++ Clever Staff Rewrite.py
@@ -13,15 +13,61 @@ print(f'1. В документе есть следующие листы -{sn}')
 # Выводим название активного листа
 print(f'2. Активный лист на данный момент -"{wb.active.title}"')
 
-# Делаем активным лист с номером 4 и выводим его название
-wb.active = 4
-print(f'3. Делаем активным лист с номером 4 и выводим его название - "{wb.active.title}"')
+# Делаем активным лист с номером 0 и выводим его название
+wb.active = 0
+print(f'3. Делаем активным лист с номером 0 и выводим его название - "{wb.active.title}"')
 
-# Узнаем количество заполненных строк на странице "кандидаты", вычетаем строку с заголовком и получаем количество кандидатов
+# Узнаем количество заполненных строк на странице "кандидаты",
+# вычетаем строку с заголовком и получаем количество кандидатов
 maxcolumn = wb['кандидаты'].max_row
 print(f'4. Количество строк на странице "кандидаты" равно - {maxcolumn}')
 NumberOfCandidates = maxcolumn - 1
 print(f'5. Количество кандидатов должно быть меньше количества строк на единицу (заголовок) и равно - {NumberOfCandidates}')
+
+# Создаем класс CandidateClass
+class Candidate:
+    def __init__(self, number, lastname, patronymic, desiredposition, currentplace, birthdate,
+                 sex, status, phone, email, skype, facebook, linkedin, typeofemployment, fieldofactivity,
+                 workexperience, salary, currency, language, languagelevel, region, dateofadding, candidateid,
+                 actiondate, actioncreator, action, commentdate, commentcreator, comment):
+        self.number = number
+        self.lastname = lastname
+        self.patronymic = patronymic
+        self.desiredposition = desiredposition
+        self.currentplace = currentplace
+        self.birthdate = birthdate
+        self.sex = sex
+        self.status = status
+        self.phone = phone
+        self.email = email
+        self.skype = skype
+        self.facebook = facebook
+        self.linkedin = linkedin
+        self.typeofemployment = typeofemployment
+        self.fieldofactivity = fieldofactivity
+        self.workexperience = workexperience
+        self.salary = salary
+        self.currency = currency
+        self.language = language
+        self.languagelevel = languagelevel
+        self.region = region
+        self.dateofadding = dateofadding
+        self.candidateid = candidateid
+        self.actiondate = actiondate
+        self.actioncreator = actioncreator
+        self.action = action
+        self.commentdate = commentdate
+        self.commentcreator = commentcreator
+        self.comment = comment
+
+a2 = wb.active['A2'].value
+print(a2)
+
+FirstCanidate = Candidate(a2, "test", "test", "test", "test", "test", "test", "test", "test", "test", "test",
+                          "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test",
+                          "test", "test", "test", "test", "test", "test", "test")
+print(FirstCanidate.number)
+        
 
 # Сохраняем проделанную работу в файл
 wb.save('AIHUB.xlsx')
