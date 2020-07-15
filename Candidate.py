4dcc80f5affc251580c3601e8a639b89676c9895
Maxim Mazin <maxim.mazin@jetbrains.com>
2020-07-15 21:48:56 +0200

    Got rid of fields that cannot be loaded from the candidates sheet

--- Candidate.py
+++ Candidate.py
@@ -1,11 +1,11 @@
 class Candidate:
     def __init__(self, number, first_name, patro_nymic, last_name, desired_position, current_position, current_place,
                  birth_date, sex, status, phone, email, skype, facebook, linkedin, type_of_employment,
-                 field_of_activity, work_experience, salary, currency, language, language_level, region, date_of_adding,
-                 local_id, action_date, action_creator, action, comment_date, comment_creator, comment):
+                 field_of_activity, work_experience, salary, currency, language, region, date_of_adding,
+                 local_id):
         self.number = number
         self.first_name = first_name
-        self.patro_nymic = patro_nymic
+        self.patronymic = patro_nymic
         self.last_name = last_name
         self.desired_position = desired_position
         self.current_position = current_position
@@ -24,19 +24,9 @@ class Candidate:
         self.salary = salary
         self.currency = currency
         self.language = language
-        self.language_level = language_level
         self.region = region
         self.date_of_adding = date_of_adding
         self.local_id = local_id
-        self.action_date = action_date
-        self.action_creator = action_creator
-        self.action = action
-        self.comment_date = comment_date
-        self.comment_creator = comment_creator
-        self.comment = comment
-
-    def __str__(self):
-        return "test"
 
     def __repr__(self) -> str:
         return self.__dict__.__str__()
--- Clever Staff Rewrite.py
+++ Clever Staff Rewrite.py
@@ -1,60 +1,52 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 
 # загружаем эксель файл который надо пересобрать
 from Candidate import Candidate
 
 
-def load_candidates(excel_filename):
-    wb = load_workbook(excel_filename)
-    candidates_sheet = wb['кандидаты']
+def load_candidates(workbook: Workbook):
+    candidates_sheet = workbook['кандидаты']
 
     # Узнаем количество заполненных строк на странице "кандидаты",
     # вычетаем строку с заголовком и получаем количество кандидатов
     number_of_candidates = candidates_sheet.max_row - 1
-    print(f'Количество кандидатов должно быть меньше количества строк на единицу (заголовок) и равно - {number_of_candidates}')
+    print(f'Количество кандидатов {number_of_candidates}')
 
     candidates = {}
     for row_number in range(2, number_of_candidates + 1):
-        number = wb.active[f'A{row_number}'].value
-        first_name = wb.active[f'B{row_number}'].value
-        patro_nymic = wb.active[f'C{row_number}'].value
-        last_name = wb.active[f'D{row_number}'].value
-        desired_position = wb.active[f'E{row_number}'].value
-        current_position = wb.active[f'F{row_number}'].value
-        current_place = wb.active[f'G{row_number}'].value
-        birth_date = wb.active[f'H{row_number}'].value
-        sex = wb.active[f'I{row_number}'].value
-        status = wb.active[f'J{row_number}'].value
-        phone = wb.active[f'K{row_number}'].value
-        email = wb.active[f'L{row_number}'].value
-        skype = wb.active[f'M{row_number}'].value
-        facebook = wb.active[f'N{row_number}'].value
-        linkedin = wb.active[f'O{row_number}'].value
-        type_of_employment = wb.active[f'P{row_number}'].value
-        field_of_activity = wb.active[f'Q{row_number}'].value
-        work_experience = wb.active[f'R{row_number}'].value
-        salary = wb.active[f'S{row_number}'].value
-        currency = wb.active[f'T{row_number}'].value
-        language = wb.active[f'U{row_number}'].value
-        language_level = wb.active[f'U{row_number}'].value
-        region = wb.active[f'V{row_number}'].value
-        date_of_adding = wb.active[f'Z{row_number}'].value
-        local_id = wb.active[f'AB{row_number}'].value
-        action_date = wb.active[f'W{row_number}'].value
-        action_creator = wb.active[f'X{row_number}'].value
-        action = wb.active[f'Y{row_number}'].value
-        comment_date = wb.active[f'Z{row_number}'].value
-        comment_creator = wb.active[f'A{row_number}'].value
-        comment = wb.active[f'B{row_number}'].value
+        number = candidates_sheet[f'A{row_number}'].value
+        first_name = candidates_sheet[f'B{row_number}'].value
+        patronymic = candidates_sheet[f'C{row_number}'].value
+        last_name = candidates_sheet[f'D{row_number}'].value
+        desired_position = candidates_sheet[f'E{row_number}'].value
+        current_position = candidates_sheet[f'F{row_number}'].value
+        current_place = candidates_sheet[f'G{row_number}'].value
+        birth_date = candidates_sheet[f'H{row_number}'].value
+        sex = candidates_sheet[f'I{row_number}'].value
+        status = candidates_sheet[f'J{row_number}'].value
+        phone = candidates_sheet[f'K{row_number}'].value
+        email = candidates_sheet[f'L{row_number}'].value
+        skype = candidates_sheet[f'M{row_number}'].value
+        facebook = candidates_sheet[f'N{row_number}'].value
+        linkedin = candidates_sheet[f'O{row_number}'].value
+        type_of_employment = candidates_sheet[f'P{row_number}'].value
+        field_of_activity = candidates_sheet[f'Q{row_number}'].value
+        work_experience = candidates_sheet[f'R{row_number}'].value
+        salary = candidates_sheet[f'S{row_number}'].value
+        currency = candidates_sheet[f'T{row_number}'].value
+        language = candidates_sheet[f'U{row_number}'].value
+        region = candidates_sheet[f'V{row_number}'].value
+        date_of_adding = candidates_sheet[f'Z{row_number}'].value
+        local_id = candidates_sheet[f'AB{row_number}'].value
         candidates[local_id] = Candidate(
-            number, first_name, patro_nymic, last_name, desired_position, current_position, current_place,
+            number, first_name, patronymic, last_name, desired_position, current_position, current_place,
             birth_date, sex, status, phone, email, skype, facebook, linkedin, type_of_employment,
-            field_of_activity, work_experience, salary, currency, language, language_level,
-            region, date_of_adding, local_id, action_date, action_creator, action, comment_date,
-            comment_creator, comment)
+            field_of_activity, work_experience, salary, currency, language,
+            region, date_of_adding, local_id)
 
     return candidates
 
 
-candidates = load_candidates('AIHUB.xlsx')
-print(candidates)
+workbook = load_workbook('AIHUB.xlsx')
+candidates = load_candidates(workbook)
+
